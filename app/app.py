2f65a93ffb4f743205267ce83409ec99a5db0fd2
boredhackerblog <38662926+BoredHackerBlog@users.noreply.github.com>
2023-07-23 22:04:41 -0400

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,39 @@
+IMAGES=["lscr.io/linuxserver/firefox:latest"]
+CONTAINER_TIME_SECONDS=30
+
+import os
+from uuid import uuid4
+from datetime import timedelta
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+REDIS_SERVER = os.environ['REDIS_SERVER']
+
+from rq import Queue
+from redis import Redis
+q = Queue(connection=Redis(REDIS_SERVER))
+
+def setup_container(image_name):
+    container_id = str(uuid4())+"-browser"
+    #send start task
+    q.enqueue('worker.create_container',args=(image_name,container_id))
+    #send stop task in 180 seconds
+    q.enqueue_in(timedelta(seconds=CONTAINER_TIME_SECONDS),'worker.kill_container',args=(container_id,))
+    return container_id
+
+# list options, captcha, user sends captcha and image name
+@app.route('/', methods=['GET', 'POST'])
+def index():
+     if request.method == 'GET':
+         return render_template("index.html", images=IMAGES)
+     if request.method == 'POST':
+         if request.form['image']:
+             image_name = request.form['image'].strip()
+             if image_name in IMAGES:
+                 container_id = setup_container(image_name)
+                 return render_template("container.html", container_id=container_id.split('-browser')[0])
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ worker/worker.py
@@ -0,0 +1,31 @@
+import docker
+
+client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+
+def create_container(image_name, uuid):
+    image = image_name
+    environment = {
+        "PUID": "1000",
+        "PGID": "1000",
+        "TZ": "Etc/UTC",
+        "SUBFOLDER": "/webtop/"
+    }
+    shm_size = "1gb"
+    network = "remotebrowsers_default"
+    container = client.containers.run(
+        image,
+        detach=True,
+        environment=environment,
+        shm_size=shm_size,
+        name=uuid,
+        network=network,
+    )
+
+    return True
+
+def kill_container(uuid):
+    container = client.containers.get(uuid)
+    container.kill()
+    container.remove()
+
+    return True
